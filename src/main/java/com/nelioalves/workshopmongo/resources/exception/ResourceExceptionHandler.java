4bc7f60ef707513d679864d7f827faea49ef19ab
Nelio Alves <acenelio@gmail.com>
2025-12-17 11:41:38 -0300

    Update Java 25 Spring Boot 4

--- src/main/java/com/nelioalves/workshopmongo/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/exception/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.nelioalves.workshopmongo.resources.exception;
 
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
