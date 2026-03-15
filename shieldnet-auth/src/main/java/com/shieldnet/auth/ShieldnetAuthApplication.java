9eee19a061b86d5624c193b8e238d224c285e6b9
Marlene Moraes <marlenevmoraes@outlook.com>
2026-03-15 18:39:10 -0300

    chore: bootstrap shieldnet-auth service

---
+++ shieldnet-auth/src/main/java/com/shieldnet/auth/ShieldnetAuthApplication.java
@@ -0,0 +1,13 @@
+package com.shieldnet.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldnetAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldnetAuthApplication.class, args);
+	}
+
+}
---
+++ shieldnet-auth/src/test/java/com/shieldnet/auth/ShieldnetAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shieldnet.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldnetAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
